7ee46c2e2990abcfce7a953676a3fbbb107bcf5a
KarishmaNair <30658314+KarishmaNair@users.noreply.github.com>
2017-08-06 13:35:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,18 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/article-1',function(req,res)
+{
+    res.send("articleone will be served");
+});
+app.get('/article-2',function(req,res)
+{
+    res.send("articletwo will be served");
+});
+app.get('/article-3',function(req,res)
+{
+    res.send("articlethree will be served");
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
